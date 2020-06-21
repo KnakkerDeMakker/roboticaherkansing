e8b99b64397ea26f5b277ffcdc46891fa20e7093
KnakkerDeMakker <63868359+KnakkerDeMakker@users.noreply.github.com>
2020-06-21 14:55:52 +0200

    Create Driver.h

---
+++ Driver.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <queue>
+#
+struct Task
+{
+	float Time; //Amount of seconds to execute task
+	float Dest; //Floating point position of destination
+};
+
+
+class Driver
+{
+	float timeRemaining;
+	std::queue<>
+
+};
