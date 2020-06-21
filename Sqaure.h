7d10df40e47e1dd3c68cd16a65c5c7ebb49470a1
KnakkerDeMakker <63868359+KnakkerDeMakker@users.noreply.github.com>
2020-06-21 14:59:16 +0200

    Create Sqaure.h

---
+++ Sqaure.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Shape.h"
+
+class Square : public Shape
+{
+	float size;
+
+public:
+	Square();
+	/*
+	 *  Creates square at pos, size is length of each side.
+	 */
+	Square(const Point2 &pos, float size);
+	Point2 TL();
+	Point2 TR();
+	Point2 BL();
+	Point2 BR();
+	void Draw() override;
+};
+
+bool test_Square();
