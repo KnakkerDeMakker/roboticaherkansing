bb30f492b672c14c667909386ff45c6083f2e662
KnakkerDeMakker <63868359+KnakkerDeMakker@users.noreply.github.com>
2020-06-21 15:00:01 +0200

    Create Time.h

---
+++ Time.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <chrono>
+#include <thread>
+
+class Time
+{
+#ifndef _WIN32
+	std::chrono::_V2::system_clock::time_point lastTime;
+#else
+	std::chrono::time_point<std::chrono::high_resolution_clock> lastTime;
+#endif
+
+public:
+	Time() : lastTime(std::chrono::high_resolution_clock::now()) {};
+	float DeltaTime;
+	void Update();
+};
+
+bool test_Time();
